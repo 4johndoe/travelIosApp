9ff8bc4b9524a1b18fce3bec4815778002b9e1bf
bogdan demchenko <bogdandemchenko@gmail.com>
2021-12-27 20:21:07 +0200

    added two view component

--- TravelDiscoveryLBTA/ContentView.swift
+++ TravelDiscoveryLBTA/ContentView.swift
@@ -14,9 +14,71 @@ struct ContentView: View {
             ScrollView{
                 
                 DiscoveryCategoriesView()
+                
+                PopularDestinationView()
+                
+                PopularRestaurantsView()
 
             }.navigationTitle("Discover")
 
+        }
+    }
+}
+
+struct PopularDestinationView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular Destination")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+                .padding(.top)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8.0) {
+                    ForEach(0..<5, id: \.self) { num in
+                        Spacer()
+                            .frame(width: 125, height: 150)
+                            .background(Color.gray)
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+            
+        }
+    }
+}
+
+struct PopularRestaurantsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular places to eat")
+                    .font(.system(size: 14, weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12, weight: .semibold))
+            }.padding(.horizontal)
+                .padding(.top)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8.0) {
+                    ForEach(0..<5, id: \.self) { num in
+                        Spacer()
+                            .frame(width: 200, height: 64)
+                            .background(Color.gray)
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+            
         }
     }
 }
