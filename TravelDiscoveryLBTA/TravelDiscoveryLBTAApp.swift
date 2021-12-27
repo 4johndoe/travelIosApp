35b6849a94943d84c47a84aa75089fa3ad357f8e
bogdan demchenko <bogdandemchenko@gmail.com>
2021-12-27 19:28:24 +0200

    Initial Commit

---
+++ TravelDiscoveryLBTA/TravelDiscoveryLBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryLBTAApp.swift
+//  TravelDiscoveryLBTA
+//
+//  Created by Bogdan Demchenko on 27.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryLBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
