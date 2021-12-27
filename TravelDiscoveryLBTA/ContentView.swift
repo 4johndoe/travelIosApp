ea3efad6447000f65681eff968a30694810feda3
bogdan demchenko <bogdandemchenko@gmail.com>
2021-12-27 19:41:14 +0200

    added basic scroll view

--- TravelDiscoveryLBTA/ContentView.swift
+++ TravelDiscoveryLBTA/ContentView.swift
@@ -9,8 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            ScrollView{
+                
+                ScrollView (.horizontal) {
+                    
+                    HStack(spacing: 16) {
+                        ForEach (0..<15, id: \.self) { num in
+                            Spacer()
+                                .frame(width: 50, height: 50)
+                                .background(Color.gray)
+                                .cornerRadius(.infinity)
+                        }
+                    }
+                }
+            }.navigationTitle("Discover")
+
+        }
     }
 }
 
