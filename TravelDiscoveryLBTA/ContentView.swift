18e17672bf2a5fad15da012d5b2e01f937511cb8
bogdan demchenko <bogdandemchenko@gmail.com>
2021-12-27 19:55:12 +0200

    extracted to subview component

--- TravelDiscoveryLBTA/ContentView.swift
+++ TravelDiscoveryLBTA/ContentView.swift
@@ -13,17 +13,8 @@ struct ContentView: View {
             
             ScrollView{
                 
-                ScrollView (.horizontal) {
-                    
-                    HStack(spacing: 16) {
-                        ForEach (0..<15, id: \.self) { num in
-                            Spacer()
-                                .frame(width: 50, height: 50)
-                                .background(Color.gray)
-                                .cornerRadius(.infinity)
-                        }
-                    }
-                }
+                DiscoveryCategoriesView()
+
             }.navigationTitle("Discover")
 
         }
@@ -33,5 +24,25 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiscoveryCategoriesView: View {
+    var body: some View {
+        ScrollView (.horizontal, showsIndicators: false) {
+            HStack(spacing: 16) {
+                ForEach (0..<5, id: \.self) { num in
+                    VStack (spacing: 4) {
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                        Text("Art")
+                            .font(.system(size: 12, weight: .semibold))
+                    }
+                }
+            }.padding(.horizontal)
+        }
     }
 }
