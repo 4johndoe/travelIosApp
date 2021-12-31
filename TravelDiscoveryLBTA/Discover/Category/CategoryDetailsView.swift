b66416af5f249cf6d21ca9d796bc680993a29aa9
bogdan demchenko <bogdandemchenko@gmail.com>
2021-12-31 23:13:22 +0200

    added debug print

--- TravelDiscoveryLBTA/Discover/Category/CategoryDetailsView.swift
+++ TravelDiscoveryLBTA/Discover/Category/CategoryDetailsView.swift
@@ -60,6 +60,7 @@ struct CategoryDetailsView: View {
     @ObservedObject private var vm: CategoryDetailsViewModel
     
     init(name: String) {
+        print("Loaded CategoryDetails View and making a network request for \(name)")
         self.name = name
         self.vm = .init(name: name)
     }
