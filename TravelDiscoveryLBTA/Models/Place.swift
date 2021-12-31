a470a532a2a21d2d0cd8e1a6219ce8bdd6d9afc2
bogdan demchenko <bogdandemchenko@gmail.com>
2021-12-31 20:40:20 +0200

    refactoring

---
+++ TravelDiscoveryLBTA/Models/Place.swift
@@ -0,0 +1,12 @@
+//
+//  Place.swift
+//  TravelDiscoveryLBTA
+//
+//  Created by Bogdan Demchenko on 31.12.2021.
+//
+
+import Foundation
+
+struct Place: Decodable, Hashable {
+    let name, thumbnail: String
+}
