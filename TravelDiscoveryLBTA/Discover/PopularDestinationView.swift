5bb8fa31a443e3e853284232ff04953c866c9dda
bogdan demchenko <bogdandemchenko@gmail.com>
2022-01-01 23:01:53 +0200

    PopularDestinationDetailsView

--- TravelDiscoveryLBTA/Discover/PopularDestinationView.swift
+++ TravelDiscoveryLBTA/Discover/PopularDestinationView.swift
@@ -51,7 +51,31 @@ struct PopularDestinationDetailsView: View {
     var body: some View{
         
         ScrollView {
-            Text("Paris")
+            Image(destination.imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(height: 200)
+                .clipped()
+            
+            VStack(alignment: .leading){
+                Text(destination.name)
+                    .font(.system(size: 18, weight: .bold))
+                Text(destination.country)
+                
+                HStack {
+                    ForEach(0..<5, id: \.self) {num in
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.orange)
+                    }
+                }.padding(.top, 2)
+                
+                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+                    .padding(.top, 4)
+                
+                HStack{ Spacer() }
+                
+            }.padding(.horizontal)
+            
         }.navigationBarTitle(destination.name, displayMode: .inline)
     }
 }
@@ -89,10 +113,10 @@ struct PopularDestinationTile: View {
 
 struct PopularDestinationView_Previews: PreviewProvider {
     static var previews: some View {
-//        DiscoverView()
+        DiscoverView()
 //        PopularDestinationView()
-        NavigationView {
-            PopularDestinationDetailsView(destination: .init(name: "Paris" ,country: "France", imageName: "eiffel_tower"))
-        }
+//        NavigationView {
+//            PopularDestinationDetailsView(destination: .init(name: "Paris" ,country: "France", imageName: "eiffel_tower"))
+//        }
     }
 }
