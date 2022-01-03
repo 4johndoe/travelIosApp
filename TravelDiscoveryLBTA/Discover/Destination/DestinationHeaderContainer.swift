2f692e601d3956c5f2ea00d27c61bcdf6ebaa0f2
bogdan demchenko <bogdandemchenko@gmail.com>
2022-01-03 21:35:59 +0200

    added swiped controllers

--- TravelDiscoveryLBTA/Discover/Destination/DestinationHeaderContainer.swift
+++ TravelDiscoveryLBTA/Discover/Destination/DestinationHeaderContainer.swift
@@ -12,13 +12,57 @@ struct DestinationHeaderContainer: UIViewControllerRepresentable {
     typealias UIViewControllerType = UIViewController
     
     func makeUIViewController(context: Context) -> UIViewController {
-        let redVC = UIViewController()
-        redVC.view.backgroundColor = .red
-        return redVC
+//        let redVC = UIHostingController(rootView: Text("First view controller"))
+////        redVC.view.backgroundColor = .red
+//        return redVC
+        let pvc = CustomPageViewController()
+        return pvc
     }
     
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         
+    }
+}
+
+class CustomPageViewController: UIPageViewController, UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = allControllers.firstIndex(of: viewController) else { return nil }
+        
+        if index == 0 { return nil }
+        
+        return allControllers[index - 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = allControllers.firstIndex(of: viewController) else { return nil }
+        
+        if index == allControllers.count - 1 { return nil }
+        
+        return allControllers[index + 1]
+    }
+    
+    
+    let firstVC = UIHostingController(rootView: Text("First"))
+    let secondVC = UIHostingController(rootView: Text("Second"))
+    let thirdVC = UIHostingController(rootView: Text("Third"))
+    
+    lazy var allControllers: [UIViewController] = [
+        firstVC, secondVC, thirdVC
+    ]
+
+    
+    init() {
+        
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        view.backgroundColor = .orange
+        
+        setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        
+        self.dataSource = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
