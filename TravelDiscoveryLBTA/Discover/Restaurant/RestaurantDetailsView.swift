42f92313ca88751e3c66514dcd5ee5bfba630fc3
bogdan demchenko <bogdandemchenko@gmail.com>
2022-01-06 23:03:57 +0200

    ups

--- TravelDiscoveryLBTA/Discover/Restaurant/RestaurantDetailsView.swift
+++ TravelDiscoveryLBTA/Discover/Restaurant/RestaurantDetailsView.swift
@@ -14,11 +14,53 @@ struct RestaurantDetailsView: View {
     var body: some View {
         ScrollView {
             
-            VStack {
+            ZStack (alignment: .bottomLeading) {
+                Image(restaurant.imageName)
+                    .resizable()
+                    .scaledToFill()
                 
+                LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black]), startPoint: .center, endPoint: .bottom)
+                
+                HStack {
+                    VStack (alignment: .leading) {
+                        Text(restaurant.name)
+                            .foregroundColor(.white)
+                            .font(.system(size: 18, weight: .bold))
+                        
+                        HStack {
+                            ForEach(0..<5, id: \.self) { num in
+                                Image(systemName: "star.fill")
+                            }.foregroundColor(.orange)
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Text("See more photos")
+                        .foregroundColor(.white)
+                        .font(.system(size: 14, weight: .regular))
+                        .frame(width: 80)
+                        .multilineTextAlignment(.trailing)
+                }.padding()
             }
             
-            Text("Details")
+            VStack(alignment: .leading) {
+                
+                Text("Location & Description")
+                    .font(.system(size: 16, weight: .bold))
+                
+                Text("Tokyo, Japan")
+                
+                HStack {
+                    ForEach(0..<5, id: \.self) { num in
+                        Image(systemName: "dollarsign.circle.fill")
+                    }.foregroundColor(.orange)
+                }
+                
+                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.")
+                    .padding(.top, 8)
+                    .font(.system(size: 14, weight: .regular))
+            }.padding()
         }
         .navigationBarTitle("Restaurant Details", displayMode: .inline)
     }
